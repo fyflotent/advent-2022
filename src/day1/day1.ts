12a988e0aa5f0867085f79b5ab473ebc1529778e
Chris Rice <chris.189.r@gmail.com>
2022-12-02 19:09:13 -0500

    Added solution for day1

--- src/day1/day1.ts
+++ src/day1/day1.ts
@@ -1,8 +1,28 @@
 import { getInput } from "../getInput";
 
+const getElves = (input: string): Array<number[]> => {
+  const splitoutInput = input.split("\r\n");
+  const elves = splitoutInput.reduce((allElves, line) => {
+    const cleanedLine = line.trim();
+    if (cleanedLine === "") {
+      return [...allElves, []];
+    } else {
+      const lastElf = allElves.pop() ?? [];
+      const parsedInt = parseInt(line);
+      if (isNaN(parsedInt)) throw Error("got nan in from line" + line);
+      return [...allElves, [...lastElf, parsedInt]];
+    }
+  }, new Array<number[]>());
+  return elves;
+};
+
 export const day1 = async (): Promise<string> => {
   const input = await getInput(1);
   console.log("calculating day1");
-  console.log(input.split("\r\n\r\n").length);
-  return "input";
+  const elves = getElves(input);
+  const elvesCalories = elves.map((elf) => {
+    return elf.reduce((partialSum, curr) => partialSum + curr, 0);
+  });
+
+  return Math.max(...elvesCalories).toString();
 };
--- src/getInput.ts
+++ src/getInput.ts
@@ -2,7 +2,7 @@ import { readFile } from "fs/promises";
 import path from "path";
 
 export const getInput = async (dayNumber: number): Promise<string> => {
-  const dayFileName = path.join("src/input", `day${dayNumber}.txt`);
+  const dayFileName = path.join("input", `day${dayNumber}.txt`);
   console.log(`loading ${dayNumber} from ${dayFileName}`);
   const inputData = await readFile(dayFileName, "utf8");
   console.log("inputData");
