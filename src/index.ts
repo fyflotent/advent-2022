cad4cfaf3c4126d478d03d81e024e24b7f815b01
Chris Rice <chris.189.r@gmail.com>
2022-12-04 00:35:15 -0500

    Day 4 solution

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { day1 } from "./days/day1";
 import { day2 } from "./days/day2";
 import { day3 } from "./days/day3";
+import { day4 } from "./days/day4";
 import { DayFunctions } from "./types";
 
 const { argv } = process;
@@ -8,6 +9,7 @@ const days: Record<string, DayFunctions> = {
   day1,
   day2,
   day3,
+  day4,
 };
 
 const runDay = async (day: number, part: 1 | 2) => {
